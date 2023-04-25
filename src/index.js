2155592fe610a2f537498b1f7c910da0022ffdfe
Iurii Sydorov <iurii.sydorov@gmail.com>
2023-04-25 15:05:47 +0300

    add ferst lesson

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import data from './data.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Card = () => {
+  return data.map(photo => {
+    return (
+      <div
+        key={photo.id}
+        className="card mx-auto my-2"
+        style={{ width: '18rem' }}
+      >
+        <img src={photo.url} className="card-img-top" alt={photo.title} />
+        <div className="card-body">
+          <h5 className="card-title">Card title: {photo.title}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="http" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  });
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Card />);
